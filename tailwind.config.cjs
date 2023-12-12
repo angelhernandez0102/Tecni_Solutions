829a75fd794abaa71aba3cf63192139549218b95
angelhernandez0102 <152232426+angelhernandez0102@users.noreply.github.com>
2023-12-11 22:48:08 -0600

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./views/**/*.pug'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+tailwind.config.js
+module.exports = {
+  content: ['./views/**/*.pug'],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'Light grayish cyan': '#d6eef2',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+
+tailwind.config.js
+module.exports = {
+  content: ['./views/**/*.pug'],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'Very dark cyan': '#03363d',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+
